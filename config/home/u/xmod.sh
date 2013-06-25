31d59e10019712f4f4befc182ee96bdcbd316fa7
mewmew <rnd0x00@gmail.com>
2013-06-25 08:24:45 +0200

    config: Update xmodmap script.

---
+++ config/home/u/xmod.sh
@@ -0,0 +1,3 @@
+xmodmap -e "keycode  76 = aring Aring aring Aring aring Aring XF86Switch_VT_10"
+xmodmap -e "keycode  95 = adiaeresis Adiaeresis adiaeresis Adiaeresis adiaeresis Adiaeresis XF86Switch_VT_11"
+xmodmap -e "keycode  96 = odiaeresis Odiaeresis odiaeresis Odiaeresis odiaeresis Odiaeresis XF86Switch_VT_12"
