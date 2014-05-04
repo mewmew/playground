766dcc72e9fe4832d8b624e729d92562c088893b
mewmew <rnd0x00@gmail.com>
2014-05-04 12:59:26 +0200

    rosa: Add fib solution.

---
+++ rosa/cmd/fib/fib.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/mewkiz/pkg/bufioutil"
+)
+
+func main() {
+	// Get input from stdin.
+	br := bufioutil.NewReader(os.Stdin)
+	nraw, err := br.ReadLine()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	n, err := strconv.Atoi(nraw)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	kraw, err := br.ReadLine()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	k, err := strconv.Atoi(kraw)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(Fib(n, k))
+}
+
+// Fib returns the total number of rabbit pairs that will be present after n
+// months if we begin with 1 pair and in each generation, every pair of
+// production-age rabbits produce a litter of k rabbit paris.
+//
+// Below follows an example where n=5 and k=3. Rabbit kits are represented using
+// r and rabbit adults using R.
+//
+// 1st month (1 rabbit)
+//    r
+//
+// 2nd month (1 rabbit)
+//    R
+//
+// 3rd month (1 + 1*3 = 4 rabbits)
+//    R r r r
+//
+// 4th month (4 + 1*3 = 7 rabbits)
+//    R R R R r r r
+//
+// 5th month (7 + 4*3 = 19 rabbits)
+//    R R R R R R R r r r r r r r r r r r r
+func Fib(n, k int) int {
+	// Similar to the Fibbonaci sequence but calculated using:
+	//    F_n = F_{n-1} = k*F_{n-2}
+	a, b := 1, 1
+	for i := 2; i < n; i++ {
+		a, b = b, b+a*k
+	}
+
+	return b
+}
---
+++ rosa/cmd/fib/fib_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func ExampleBaseCount() {
+	n := 5
+	k := 3
+	fmt.Println(Fib(n, k))
+	// Output: 19
+}
