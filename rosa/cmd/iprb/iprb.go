bc1d03ed4eaa04218bcf1a32ad38562fcd35ffb2
mewmew <rnd0x00@gmail.com>
2014-05-12 01:00:01 +0200

    rosa: Optimize iprb.

--- rosa/cmd/iprb/iprb.go
+++ rosa/cmd/iprb/iprb.go
@@ -29,22 +29,23 @@ func DominantProb(k, m, n int) (prob float64) {
 	K, M, N := float64(k), float64(m), float64(n)
 	total := K + M + N
 
+	// Dr+Dr: 25% recessive.
+	recessive := 0.25 * M * (M - 1)
+
+	// Dr+rr: 50% recessive.
+	recessive += 0.5 * M * N
+
+	// rr+Dr: 50% recessive.
+	recessive += 0.5 * N * M
+
+	// rr+rr: 100% recessive.
+	recessive += N * (N - 1)
+
 	// The probability of producing an individual possessing a dominant allele is
 	// calculated by subtracting the chance of a homozygous recessive offspring
 	// from an initial probability of 100%.
-	prob = 1
-
-	// Dr+Dr: 25% recessive.
-	prob -= 0.25 * M / total * (M - 1) / (total - 1)
-
-	// Dr+rr: 50% recessive.
-	prob -= 0.50 * M / total * N / (total - 1)
-
-	// rr+Dr: 50% recessive.
-	prob -= 0.50 * N / total * M / (total - 1)
-
-	// rr+rr: 100% recessive.
-	prob -= 1.00 * N / total * (N - 1) / (total - 1)
+	recessive /= total * (total - 1)
+	prob = 1 - recessive
 
 	return prob
 }
