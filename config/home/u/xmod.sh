ac28fffaf566876553a11f685016d44a93dc533e
mewmew <rnd0x00@gmail.com>
2016-03-09 13:50:44 +0100

    config: Add Menu keybinding in between Ctrl and Alt on the right side of the keyboard.

--- config/home/u/xmod.sh
+++ config/home/u/xmod.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 xmodmap -e "keycode  76 = aring Aring aring Aring aring Aring XF86Switch_VT_10"
 xmodmap -e "keycode  95 = adiaeresis Adiaeresis adiaeresis Adiaeresis adiaeresis Adiaeresis XF86Switch_VT_11"
 xmodmap -e "keycode  96 = odiaeresis Odiaeresis odiaeresis Odiaeresis odiaeresis Odiaeresis XF86Switch_VT_12"
+xmodmap -e "keycode  107 = Menu NoSymbol Menu"
