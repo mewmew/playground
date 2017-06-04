4ff54c3c95f99c43e878f242480a12b5b7f915f8
mewmew <rnd0x00@gmail.com>
2017-06-04 12:42:09 +0200

    cmd/dela: add preliminary upload functionality

--- cmd/dela/dela.go
+++ cmd/dela/dela.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/kr/pretty"
+	"github.com/pkg/errors"
 )
 
 func logger(handler http.Handler) http.Handler {
@@ -15,11 +21,64 @@ func logger(handler http.Handler) http.Handler {
 }
 
 func main() {
-	var addr string
-	flag.StringVar(&addr, "http", ":8080", "HTTP service address (e.g., ':6060')")
+	var (
+		addr string
+		recv bool
+	)
+	flag.StringVar(&addr, "http", ":8080", "HTTP service address")
+	flag.BoolVar(&recv, "recv", false, "enable upload handler")
+	flag.BoolVar(&recv, "max", false, "enable upload handler")
 	flag.Parse()
-	handler := logger(http.FileServer(http.Dir(".")))
+	var handler http.Handler
+	if recv {
+		handler = logger(http.HandlerFunc(upload))
+	} else {
+		handler = logger(http.FileServer(http.Dir(".")))
+	}
 	if err := http.ListenAndServe(addr, handler); err != nil {
 		log.Fatal(err)
 	}
 }
+
+// upload handles upload requests.
+func upload(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		if _, err := fmt.Fprintf(w, uploadPage[1:]); err != nil {
+			log.Fatalf("%+v", errors.WithStack(err))
+		}
+		return
+	}
+	if err := r.ParseMultipartForm(1024 * 1024 * 1024); err != nil {
+		log.Fatalf("%+v", errors.WithStack(err))
+	}
+	file, hdr, err := r.FormFile("file")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	pretty.Println("hdr:", hdr.Header)
+	f, err := ioutil.TempFile("/tmp", "upload_")
+	if err != nil {
+		log.Fatalf("%+v", errors.WithStack(err))
+	}
+	defer f.Close()
+	if _, err := io.Copy(f, file); err != nil {
+		log.Fatalf("%+v", errors.WithStack(err))
+	}
+}
+
+const uploadPage = `
+<!doctype html>
+<html>
+	<head>
+		<title>upload file</title>
+	</head>
+	<body>
+		<form enctype="multipart/form-data" action="/" method="POST">
+			<input type="file" name="file">
+			<input type="submit" value="upload">
+		</form>
+	</body>
+</html>
+`
