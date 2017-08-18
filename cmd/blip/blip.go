3b751221b39af170c69a41e0e6574a7eed8527c8
mewmew <rnd0x00@gmail.com>
2017-08-18 20:31:30 +0200

    cmd/blip: add blip tool

---
+++ cmd/blip/blip.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/flac"
+	"github.com/faiface/beep/speaker"
+)
+
+func main() {
+	flag.Parse()
+	if flag.NArg() != 1 {
+		fmt.Fprintf(os.Stderr, "Usage: blip FILE\n")
+		os.Exit(1)
+	}
+	fr, err := os.Open(flag.Arg(0))
+	if err != nil {
+		log.Fatalf("unable to open file; %v", err)
+	}
+	s, format, err := flac.Decode(fr)
+	if err != nil {
+		log.Fatalf("unable to decode file; %v", err)
+	}
+	defer s.Close()
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	done := make(chan struct{})
+	f := func() {
+		close(done)
+	}
+	notify := beep.Callback(f)
+	speaker.Play(beep.Seq(s, notify))
+	select {
+	case <-done:
+		break
+	}
+}
