b88bd174d31a5cf106e75ad13a7785a26c19e717
Robin Eklind <rnd0x00@gmail.com>
2018-09-28 15:28:20 +0200

    cmd/lyrics: add lyrics command

---
+++ cmd/lyrics/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+	"log"
+	"strings"
+
+	"github.com/rhnvrm/lyric-api-go"
+)
+
+func main() {
+	// Parse command line arguments.
+	var (
+		// Artist name.
+		artist string
+		// Song name.
+		song string
+	)
+	const (
+		defaultArtist = "Johnossi"
+		defaultSong = "What's the point"
+	)
+	flag.StringVar(&artist, "artist", defaultArtist, "artist name")
+	flag.StringVar(&song, "song", defaultSong, "song name")
+	flag.Parse()
+	if artist == defaultArtist && song == defaultSong {
+		args := flag.Args()
+		switch flag.NArg() {
+		case 1:
+			parts := strings.Split(args[0], "-")
+			artist = parts[0]
+			song = parts[1]
+		case 2:
+			artist = args[0]
+			song = args[1]
+		}
+	}
+
+	// Search for lyric.
+	l := lyrics.New()
+	lyric, err := l.Search(artist, song)
+	if err != nil {
+		log.Fatalf("unable to locate lyrics for `%v - %v`", artist, song)
+	}
+	fmt.Println(lyric)
+}
