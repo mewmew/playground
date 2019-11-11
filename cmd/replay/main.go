dd1da389bfeec179ae2c91f20dd9798bbbd8ce72
Robin Eklind <rnd0x00@gmail.com>
2019-11-11 17:38:59 -0600

    cmd/replay: restore support for downloading playlist URLs

--- cmd/replay/main.go
+++ cmd/replay/main.go
@@ -54,14 +54,17 @@ func replay(url, output string) error {
 	if err != nil {
 		return errors.WithStack(err)
 	}
+	fmt.Println("title:", title)
+	fmt.Println("wid:", wid)
+	fmt.Println("entryID:", entryID)
 	if len(output) == 0 {
 		output = fmt.Sprintf("%s.mp4", title)
 	}
-	playlist, err := downloadPlaylist(wid, entryID)
+	playlist, flavorID, err := downloadPlaylist(wid, entryID)
 	if err != nil {
 		return errors.WithStack(err)
 	}
-	if err := downloadSegments(playlist, output); err != nil {
+	if err := downloadSegments(playlist, wid, entryID, flavorID, output); err != nil {
 		return errors.WithStack(err)
 	}
 	return nil
@@ -104,18 +107,56 @@ func part(s, before, after string) (string, error) {
 }
 
 // downloadPlaylist downloads the playlist of the given wid and entryID.
-func downloadPlaylist(wid, entryID string) (string, error) {
-	resolution := "0_63zp42a8"
-	playlistURL := fmt.Sprintf("https://streaming.kaltura.nordu.net/hls/p/%s/sp/30800/serveFlavor/entryId/%s/v/2/ev/4/flavorId/%s/name/a.mp4/index.m3u8", wid, entryID, resolution)
+func downloadPlaylist(wid, entryID string) (playlistURL string, flavorID string, err error) {
+	// ?referrer=aHR0cHM6Ly9wbGF5Lmt0aC5zZQ==&ks=djJ8MzA4fEWcfphzmX-VuTM6vT14leOa6B_LM_23ohXmu9Ttp4Az_QzXrxktLlpkOLRm6YEqk1AYC811o2MNXmyFT2DM5wu0IWRrrBnL6BR1vH-ILuC7N_XCX_K_2yalPQsrEks_oWBP38obfeBX1Q5xAAzOIDYWqYGOXYXWIGCkKOGvwyjmwLjgg514orXmxafcL3jryy28gmEoG_NeBekgRvY1M3SkjkVokP4pWpCW6lVjt8XjZhg7GzsmrAXCHIbAwm8NXZTpvkxa7_uHCzHRrcKV3Qq1jQk4ewvFnzT1vWJxQwkY2MunRyNRDT_Z_VJyuPwtR0NeO3QGqD6uJuOiEmQBcXoFO-RhV3FbKi7CAlf4kNjw_HyL64OHee6KH21wgykZtY4cCB5VpR-pARmzAAeBAdU=&playSessionId=e971d799-545a-08cb-d178-8129c2d34df8&clientTag=html5:v2.78.2&uiConfId=23449749&responseFormat=jsonp&callback=jQuery11110015037160142873152_1573514027666&_=1573514027667
+	// skip 0_f8q2jkxy (640x360)
+	url := fmt.Sprintf("https://api.kaltura.nordu.net/p/%s/sp/%s00/playManifest/entryId/%s/flavorIds/0_82oamv32,0_ansm9wom/format/applehttp/protocol/https/a.m3u8", wid, wid, entryID)
+	fmt.Println("get list of playlists:", url)
+	playlists, err := httputil.GetString(url)
+	if err != nil {
+		return "", "", errors.WithStack(err)
+	}
+	s := bufio.NewScanner(strings.NewReader(playlists))
+	// Locate playlist URL with highest resolution.
+	//
+	// Example
+	//
+	//    #EXTM3U
+	//    #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=720841,RESOLUTION=640x360
+	//    https://streaming.kaltura.nordu.net/hls/p/308/sp/30800/serveFlavor/entryId/0_wzdrx1x6/v/2/ev/4/flavorId/0_f8q2jkxy/name/a.mp4/index.m3u8
+	//    #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=1722112,RESOLUTION=1024x576
+	//    https://streaming.kaltura.nordu.net/hls/p/308/sp/30800/serveFlavor/entryId/0_wzdrx1x6/v/2/ev/4/flavorId/0_82oamv32/name/a.mp4/index.m3u8
+	//    #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=4346134,RESOLUTION=1920x1080
+	//    https://streaming.kaltura.nordu.net/hls/p/308/sp/30800/serveFlavor/entryId/0_wzdrx1x6/v/2/ev/4/flavorId/0_ansm9wom/name/a.mp4/index.m3u8
+	for s.Scan() {
+		line := s.Text()
+		if !strings.HasPrefix(line, "https://") {
+			continue
+		}
+		// select last playlist URL as it will have the highest resolution.
+		playlistURL = line
+	}
+	if err := s.Err(); err != nil {
+		return "", "", errors.WithStack(err)
+	}
+	if len(playlistURL) == 0 {
+		return "", "", errors.Errorf("unable to locate playlist URL in %q", playlists)
+	}
+	flavorID, err = part(playlistURL, "flavorId/", "/name")
+	if err != nil {
+		return "", "", errors.Errorf("unable to extract flavorID from from playlist URL %q; %v", playlistURL, err)
+	}
+	fmt.Println("flavorID:", flavorID)
+	fmt.Println("get playlist:", playlistURL)
 	playlist, err := httputil.GetString(playlistURL)
 	if err != nil {
-		return "", errors.WithStack(err)
+		return "", "", errors.WithStack(err)
 	}
-	return playlist, nil
+	return playlist, flavorID, nil
 }
 
 // downloadSegments downloads the video segments of the given playlist.
-func downloadSegments(playlist, output string) error {
+func downloadSegments(playlist, wid, entryID, flavorID, output string) error {
 	// Parse m3u playlist.
 	s := bufio.NewScanner(strings.NewReader(playlist))
 	var urls []string
@@ -138,13 +179,16 @@ func downloadSegments(playlist, output string) error {
 		if !strings.HasPrefix(seg, "seg-") {
 			continue
 		}
-		url := "https://streaming.kaltura.nordu.net/hls/p/308/sp/30800/serveFlavor/entryId/0_91vao47k/v/2/ev/4/flavorId/0_lzx3ynr5/name/a.mp4/" + seg
+		fmt.Println("segment:", seg)
+		url := fmt.Sprintf("https://streaming.kaltura.nordu.net/hls/p/%s/sp/%s00/serveFlavor/entryId/%s/v/2/ev/4/flavorId/%s/name/a.mp4/%s", wid, wid, entryID, flavorID, seg)
 		urls = append(urls, url)
 	}
 	if err := s.Err(); err != nil {
 		return errors.WithStack(err)
 	}
-
+	if len(urls) == 0 {
+		return errors.Errorf("unable to locate segments in %q", playlist)
+	}
 	var segNames []string
 	for i, url := range urls {
 		segName := segmentName(url)
