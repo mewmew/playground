d9e01d5ea04565c6812ba4e3c439a021d45d6028
Robin Eklind <rnd0x00@gmail.com>
2021-05-05 12:40:55 +0200

    archive/ted/cmd/order: view TED talk directly in browser if download link is missing

--- archive/ted/cmd/order/order.go
+++ archive/ted/cmd/order/order.go
@@ -5,12 +5,13 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/mewmew/playground/archive/ted"
+	"github.com/pkg/errors"
 )
 
 func main() {
@@ -18,45 +19,65 @@ func main() {
 	for _, path := range flag.Args() {
 		talks, err := parse(path)
 		if err != nil {
-			log.Fatalln(err)
+			log.Fatalf("%+v", err)
 		}
-		order(talks)
+		if err := order(talks); err != nil {
+			log.Fatalf("%+v", err)
+		}
 	}
 }
 
+// files tracks open files.
+var files = make(map[string]*os.File)
+
 // order outputs one wget script for downloading TED talks per release month.
-func order(talks []ted.Talk) {
-	var prev string
-	var w io.Writer
+func order(talks []ted.Talk) error {
 	for _, talk := range talks {
 		date := talk.Date.Format("2006-01")
-		if prev == "" || prev != date {
-			f, err := os.Create(fmt.Sprintf("%s.sh", date))
+		shPath := fmt.Sprintf("%s.sh", date)
+		file, ok := files[shPath]
+		if !ok {
+			fmt.Printf("Creating %q\n", shPath)
+			f, err := os.Create(shPath)
 			if err != nil {
-				log.Fatalln(err)
+				return errors.WithStack(err)
 			}
-			err = f.Chmod(0755)
-			if err != nil {
-				log.Fatalln(err)
+			if err := f.Chmod(0755); err != nil {
+				return errors.WithStack(err)
 			}
-			defer f.Close()
-			w = f
-			fmt.Fprintf(w, "mkdir -p %q\n", date)
+			file = f
+			files[shPath] = file
+			fmt.Fprint(file, "#!/bin/bash\n\n")
+			fmt.Fprintf(file, "mkdir -p %q\n\n", date)
 		}
-		fmt.Fprintf(w, "wget -O \"%s/%s (%s) [%v].mp4\" %s\n", date, talk.Title, talk.Event, talk.Duration, talk.Download)
-		prev = date
+		if len(talk.Download) > 0 {
+			fmt.Fprintf(file, "wget -O \"%s/%s (%s) [%v].mp4\" %q\n", date, escape(talk.Title), escape(talk.Event), talk.Duration, talk.Download)
+		} else {
+			fmt.Fprintf(file, "# firefox %q\n", talk.Link)
+		}
 	}
+	for _, file := range files {
+		if err := file.Close(); err != nil {
+			return errors.WithStack(err)
+		}
+	}
+	return nil
 }
 
 // parse parses the provided JSON file of TED talks.
 func parse(path string) (talks []ted.Talk, err error) {
 	buf, err := ioutil.ReadFile(path)
 	if err != nil {
-		return nil, err
+		return nil, errors.WithStack(err)
 	}
 	err = json.Unmarshal(buf, &talks)
 	if err != nil {
-		return nil, err
+		return nil, errors.WithStack(err)
 	}
 	return talks, nil
+}
+
+// escape escapes double quotes.
+func escape(s string) string {
+	return strings.ReplaceAll(s, `"`, `\"`)
 }
